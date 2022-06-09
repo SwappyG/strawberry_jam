96da3c90d9779029679c1195cf788480ca1a72b2
Swapnil Patel <swapnilpatel95@gmail.com>
2022-06-09 18:00:57 -0400

    create accesspoint for app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { StrawberryJam } from "./StrawberryJam.js"
 import { DiscordClient } from "./DiscordClient.js"
 
+import express from "express"
+
+
 const App = () => {
   const args = {
     'discord_token_file_path': 'keys/discord_token.json',
   }
+
+  const app = express()
+  const port = process.env.PORT || 5000
+
+  app.get('/', (req, res) => res.send(`SwappyJam Discord Bot`))
+  app.listen(port, () => console.log(`Listening on ${port}`))
 
   const letter_jam = new StrawberryJam(new DiscordClient(args))
 }
